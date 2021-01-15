0020dc58c6efd2925fdf79461126e88c56b8625c
Mateo Riofrio <materiochi123@gmail.com>
2021-01-15 17:11:09 -0500

    add push and pop methods

--- src/main/python/datastructures/stack/stack.py
+++ src/main/python/datastructures/stack/stack.py
@@ -11,3 +11,13 @@ class Stack:
     def size(self):
         """Return the size of the stack."""
         return self.lst.size()
+    
+    def push(self, data):
+        """Add an element to front of the linked list representing the stack."""
+        self.lst.add_first(data)
+
+    def pop(self):
+        """Remove the top element from the linked list representing the stack."""
+        return self.lst.pop()        
+    
+    
