29f753a0219547e48a818ae50a8222e80b6b8a43
Mateo Riofrio <materiochi123@gmail.com>
2021-01-16 10:26:03 -0500

    finish tests for doubly linked list

--- src/test/python/datastructures/linkedlist/doubly_linked_list_test.py
+++ src/test/python/datastructures/linkedlist/doubly_linked_list_test.py
@@ -8,20 +8,54 @@ class TestLinkedList(unittest.TestCase):
         self.lst = DoublyLinkedList()
         self.MAX_RAND_NUM = 250
         self.TEST_SZ = 50
-        self.rand_elem = random.randrange(self.MAX_RAND_NUM)
         
     def test_size(self):
-        # test empty linked list size method
+        # test empty and of size 1 
         self.assertEqual(self.lst.size(), 0)
+        self.lst.add(2)
+        self.assertEqual(self.lst.size(), 1)
 
+        # random size 
         for i in range(self.TEST_SZ):
             self.lst.add(i)
         self.assertEqual(self.lst.size(), self.TEST_SZ)
     
     def test_is_empty(self):
         self.assertTrue(self.lst.is_empty())
-        self.lst.add(random.randrange(self.MAX_RAND_NUM))
+        self.lst.add(3)
         self.assertFalse(self.lst.is_empty())
+    
+    def test_add_first(self):
+        self.lst.add_first(5)
+        self.assertTrue(self.lst.size(), 1)
+    
+    def test_add_last(self):
+        self.lst.add_last(3)
+        self.assertTrue(self.lst.size(), 1)
+    
+    def test_pop(self):
+        # test empty list
+        self.assertIsNone(self.lst.pop())
+
+        # test smallest non empty list
+        self.lst.add(3)
+        self.assertEqual(self.lst.pop(), 3)
+
+        # test random size array
+        rand_list = random.sample(range(self.MAX_RAND_NUM), self.TEST_SZ)
+        first_elem = rand_list[0]
+        self.assertEqual(self.lst.pop(), first_elem)
+    
+    def test_poll(self):
+        self.assertIsNone(self.lst.poll())
+
+        self.lst.add(5)
+        self.assertEqual(self.lst.poll(), 5)
+
+        rand_list = random.sample(range(self.MAX_RAND_NUM), self.TEST_SZ)
+        last_elem = rand_list[self.TEST_SZ - 1]
+        self.assertEqual(self.lst.pop(), last_elem)
+        
         
 if __name__ == '__main__':
     unittest.main()
