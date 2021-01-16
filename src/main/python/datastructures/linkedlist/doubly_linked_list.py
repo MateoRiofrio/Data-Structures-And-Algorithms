0874234ec5fcb3b496074b8a8f650f27f86ce1d5
Mateo Riofrio <materiochi123@gmail.com>
2021-01-15 22:59:15 -0500

    implement add_first method

--- src/main/python/datastructures/linkedlist/doubly_linked_list.py
+++ src/main/python/datastructures/linkedlist/doubly_linked_list.py
@@ -23,3 +23,27 @@ class DoublyLinkedList:
         self.top_sentinel = Node() 
         self.bottom_sentinel = Node() 
         self.length = 0
+    
+    def is_empty(self):
+        return self.length == 0
+
+    def size(self):
+        return self.length
+    
+    def add_first(self, node):
+        """Add node to the top of the doubly linked list."""
+        if(self.is_empty() == True):
+            # node points 'prev' to the top sentinel and 'next_' to the bottom sentinel
+            new_node = Node(node, self.top_sentinel, self.bottom_sentinel)
+            self.top_sentinel.next = new_node
+            self.bottom_sentinel.prev = new_node
+        else:
+            # node points 'prev' to top sentinel and 'next_' to the old top node  
+            new_node = Node(node, self.top_sentinel, self.top_sentinel.next)
+            self.top_sentinel.next.prev = new_node
+            self.top_sentinel.next = new_node
+    
+        self.length += 1
+
+    # 'add' has same functionality as add_first
+    add = add_first
