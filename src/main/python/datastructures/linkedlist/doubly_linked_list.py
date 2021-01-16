a96335ac79445f69668878f9a9ffd136db64a932
Mateo Riofrio <materiochi123@gmail.com>
2021-01-16 10:51:22 -0500

    implement add_last, poll, and pop methods

--- src/main/python/datastructures/linkedlist/doubly_linked_list.py
+++ src/main/python/datastructures/linkedlist/doubly_linked_list.py
@@ -1,8 +1,8 @@
 class Node:
     "Doubly Linked List Node that holds data, next node, and previous, node."
-    def __init__(self, data = None, foward = None, prev = None):
+    def __init__(self, data = None, prev = None, next_= None):
         self.data = data
-        self.next = foward
+        self.next = next_
         self.prev = prev
 
     def __str__(self):
@@ -11,7 +11,7 @@ class Node:
 class DoublyLinkedList:
     """Doubly linked list using nodes.
     
-    __init__ method creates a sentinel node and initializes the length of the list to 0.
+    __init__ takes creates a top and bottom sentinel and initializes the length of the list to 0.
     
     Attributes:
         top_sentinel (Node): empty node that is always at the beginning of the linked list.
@@ -45,5 +45,52 @@ class DoublyLinkedList:
     
         self.length += 1
 
-    # 'add' has same functionality as add_first
+    # 'add' has same functionality as 'add_first'
     add = add_first
+
+    def add_last(self, node):
+        """Add node to the bottom of the doubly linked list."""
+        if(self.is_empty() == True):
+            # node points to top and bottom sentinel
+            new_last_node = Node(self.top_sentinel, self.bottom_sentinel)
+            self.top_sentinel = new_last_node
+            self.bottom_sentinel.prev = new_last_node
+        else:
+            # node points to old bottom node and bottom sentinel
+            new_node = Node(node, self.bottom_sentinel.prev, self.bottom_sentinel)
+            self.bottom_sentinel.prev.next = new_node
+            self.bottom_sentinel.prev = new_node
+        
+        self.length += 1
+    
+    def pop(self):
+        """Remove the item at the top of the list."""
+        if(self.is_empty() == True):
+            return None
+
+        # assign variables to the target node and the new top node
+        target_node = self.top_sentinel.next
+        new_top_node = target_node.next
+
+        # point the new top node
+        self.top_sentinel.next = new_top_node
+        new_top_node.prev = self.top_sentinel
+        self.length -= 1
+
+        return target_node.data
+    
+    def poll(self):
+        """Remove the item at the bottom of the list."""
+        if(self.is_empty() == True):
+            return None
+
+        # assign variables to the target node and the node before the target node. 
+        target_node = self.bottom_sentinel.prev
+        new_bottom_node = target_node.prev
+
+        # point to new bottom node
+        self.bottom_sentinel.prev = new_bottom_node
+        new_bottom_node.next = self.bottom_sentinel
+        self.length -= 1
+
+        return target_node.data
