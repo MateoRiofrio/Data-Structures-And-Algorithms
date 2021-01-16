eb4973c66876189ebcda1b3d4d426be712db3ea6
Mateo Riofrio <materiochi123@gmail.com>
2021-01-16 13:49:53 -0500

    implement queue based on tests, fix some issues with test

---
+++ src/main/python/datastructures/queue/queue.py
@@ -0,0 +1,20 @@
+from doubly_linked_list import DoublyLinkedList
+class Queue:
+    def __init__(self):
+        self.lst = DoublyLinkedList()
+        self.length = 0
+    
+    def is_empty(self):
+        return self.length == 0
+    
+    def size(self):
+        return self.length
+    
+    def enqueue(self, data):
+        self.lst.add(data)
+        self.length += 1
+    
+    def dequeue(self):
+        self.length -=1
+        return self.lst.poll()
+             
--- src/test/python/datastructures/queue/queue_test.py
+++ src/test/python/datastructures/queue/queue_test.py
@@ -1,5 +1,5 @@
 import unittest
-from queue import Queue
+from src.main.python.datastructures.queue.queue import Queue
 import random
 
 class TestQueue(unittest.TestCase):
@@ -14,54 +14,45 @@ class TestQueue(unittest.TestCase):
         self.assertTrue(self.queue.is_empty())
 
         # smallest non-empty queue
-        self.queue.push(3)
+        self.queue.enqueue(3)
         self.assertFalse(self.queue.is_empty())
 
     def test_size(self):
         # empty and smallest non-empty queue
         self.assertEqual(self.queue.size(), 0)
-        self.queue.push(5)
+        self.queue.enqueue(5)
         self.assertEqual(self.queue.size(), 1)
         
         # higher bound size 
         for elem in range(self.TEST_SZ):
-            self.queue.push(elem)
+            self.queue.enqueue(elem)
         self.assertEqual(self.queue.size(), self.TEST_SZ + 1)
     
-    def test_pop(self):
+    def test_dequeue(self):
         # test empty list
-        self.assertIsNone(self.queue.pop())
+        self.assertIsNone(self.queue.dequeue())
         
         # smallest non empty list
-        self.queue.push(2)
-        self.assertEqual(self.queue.pop(), 2)
+        self.queue.enqueue(2)
+        self.assertEqual(self.queue.dequeue(), 2)
 
-        # insert 50 random elements and test pop correctness
-        rand_list = random.sample(range(self.MAX_RAND_NUM), self.TEST_SZ)
-        target_elem = rand_list[self.TEST_SZ - 1]
-        for elem in rand_list:
-            self.queue.push(elem)
-        self.assertEqual(self.queue.pop(), target_elem)
-    
-    def test_poll(self):
-        # test empty list
-        self.assertIsNone(self.queue.poll())
-        
-        # smallest non-empty list
-        self.queue.push(2)
-        self.assertEqual(self.queue.poll(), 2)
-
-        # insert 50 random elements and test poll correctness
+        # insert 50 random elements and test dequeue correctness
         rand_list = random.sample(range(self.MAX_RAND_NUM), self.TEST_SZ)
         target_elem = rand_list[0]
         for elem in rand_list:
-            self.queue.push(elem)
-        self.assertEqual(self.queue.poll(), target_elem)
+            self.queue.enqueue(elem)
+        self.assertEqual(self.queue.dequeue(), target_elem)
 
-    def test_push(self):
-        self.queue.push(4)
+    def test_enqueue(self):
+        self.queue.enqueue(4)
         self.assertEqual(self.queue.size(), 1)
-        self.assertEqual(self.queue.pop(), 4)    
+        self.assertEqual(self.queue.dequeue(), 4)  
+
+        # insert 50 random elements and test pop correctness
+        rand_list = random.sample(range(self.MAX_RAND_NUM), self.TEST_SZ)
+        for elem in rand_list:
+            self.queue.enqueue(elem)
+        self.assertEqual(self.queue.size(), self.TEST_SZ) 
 
 if __name__ == '__main__':
     unittest.main()
